dac9ae1e9dd2a42a5a9f16c9a4d275406e141e48
zhm-real <zhou.hm0420@gmail.com>
2020-07-29 17:05:05 -0700

    update

--- quintic_polynomial.py
+++ quintic_polynomial.py
@@ -6,7 +6,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-from CurvesGenerator import draw
+import draw
 
 
 class QuinticPolynomial:
--- reeds_shepp.py
+++ reeds_shepp.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
+import draw
 
 # parameters initiation
 STEP_SIZE = 0.2
@@ -11,13 +13,13 @@ PI = math.pi
 # class for PATH element
 class PATH:
     def __init__(self, lengths, ctypes, L, x, y, yaw, directions):
-        self.lengths = lengths              # lengths of each part of path (+: forward, -: backward) [float]
-        self.ctypes = ctypes                # type of each part of the path [string]
-        self.L = L                          # total path length [float]
-        self.x = x                          # final x positions [m]
-        self.y = y                          # final y positions [m]
-        self.yaw = yaw                      # final yaw angles [rad]
-        self.directions = directions        # forward: 1, backward:-1
+        self.lengths = lengths  # lengths of each part of path (+: forward, -: backward) [float]
+        self.ctypes = ctypes  # type of each part of the path [string]
+        self.L = L  # total path length [float]
+        self.x = x  # final x positions [m]
+        self.y = y  # final y positions [m]
+        self.yaw = yaw  # final yaw angles [rad]
+        self.directions = directions  # forward: 1, backward:-1
 
 
 def calc_optimal_path(sx, sy, syaw, gx, gy, gyaw, maxc, step_size=STEP_SIZE):
@@ -662,3 +664,57 @@ def check_path(sx, sy, syaw, gx, gy, gyaw, maxc):
 
         for i in range(len(d)):
             assert abs(d[i] - STEP_SIZE) <= 0.001
+
+
+def main():
+    # choose states pairs: (x, y, yaw)
+    # simulation-1
+    # states = [(0, 0, 0), (10, 10, -90), (20, 5, 60), (30, 10, 120),
+    #           (35, -5, 30), (25, -10, -120), (15, -15, 100), (0, -10, -90)]
+
+    # simulation-2
+    states = [(-3, 3, 120), (10, -7, 30), (10, 13, 30), (20, 5, -25),
+              (35, 10, 180), (32, -10, 180), (5, -12, 90)]
+
+    max_c = 0.1  # max curvature
+    path_x, path_y, yaw = [], [], []
+
+    for i in range(len(states) - 1):
+        s_x = states[i][0]
+        s_y = states[i][1]
+        s_yaw = np.deg2rad(states[i][2])
+        g_x = states[i + 1][0]
+        g_y = states[i + 1][1]
+        g_yaw = np.deg2rad(states[i + 1][2])
+
+        path_i = calc_optimal_path(s_x, s_y, s_yaw,
+                                   g_x, g_y, g_yaw, max_c)
+
+        path_x += path_i.x
+        path_y += path_i.y
+        yaw += path_i.yaw
+
+    # animation
+    plt.ion()
+    plt.figure(1)
+
+    for i in range(len(path_x)):
+        plt.clf()
+        plt.plot(path_x, path_y, linewidth=1, color='gray')
+
+        for x, y, theta in states:
+            draw.Arrow(x, y, np.deg2rad(theta), 2, 'blueviolet')
+
+        draw.Car(path_x[i], path_y[i], yaw[i], 1.5, 3)
+
+        plt.axis("equal")
+        plt.title("Simulation of Reeds-Shepp Curves")
+        plt.axis([-10, 42, -20, 20])
+        plt.draw()
+        plt.pause(0.001)
+
+    plt.pause(1)
+
+
+if __name__ == '__main__':
+    main()
